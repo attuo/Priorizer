fe1b05676ffe68491b580ece398a18317e9fd32e
attuo <attuo@protonmail.com>
2020-10-05 21:01:21 +0300

    Change text field component to be more simple

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,31 +1,115 @@
-import React from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { Draggable } from 'react-beautiful-dnd';
-import Text from "./Text";
-import { XSquare } from 'react-feather';
+import { Edit, CheckSquare, XSquare } from 'react-feather';
+import useKeypress from "../hooks/useKeyPress";
+import useOnClickOutside from "../hooks/useOnClickOutside";
 
 import './Todo.css'
 
 function Todo(props) {
+
   // TODO: Use hooks to NOT pass the "remove" props all the way down here
   // TODO: Don't render all the changes all the time
   const { data, areaKey, index, remove, change } = props;
 
+  const [isInputActive, setIsInputActive] = useState(false);
+  const [inputValue, setInputValue] = useState(data.text);
+
+  const wrapperRef = useRef(null);
+  const textRef = useRef(null);
+  const inputRef = useRef(null);
+
+  const enter = useKeypress("Enter");
+  const esc = useKeypress("Escape");
+
+
+  // Click outside
+  // useOnClickOutside(wrapperRef, () => {
+  //   if (isInputActive) {
+  //     onSetText(inputValue);
+  //     setIsInputActive(false);
+  //   }
+  // });
+
+  const onEnter = useCallback(() => {
+    if (enter) {
+      change(areaKey, index, inputValue);
+      setIsInputActive(false);
+    }
+  }, [enter, inputValue, change]);
+
+  const onEsc = useCallback(() => {
+    if (esc) {
+      setInputValue(data.text);
+      setIsInputActive(false);
+    }
+  }, [esc, data.text]);
+
+  // focus the cursor when edit is active
+  useEffect(() => {
+    if (isInputActive) {
+      inputRef.current.focus();
+    }
+  }, [isInputActive]);
+
+  useEffect(() => {
+    if (isInputActive) {
+      onEnter(); // save text changes 
+      onEsc(); // revert text changes
+    }
+  }, [onEnter, onEsc, isInputActive]); // watch the Enter and Escape key presses
+
+  const handleInputChange = useCallback(
+    (event) => {
+      // TODO: Sanitize input
+      setInputValue(event.target.value);
+    },
+    [setInputValue]
+  );
+
+  const handleSave = () => {
+    change(areaKey, index, inputValue);
+    setIsInputActive(false);
+  }
+
+  const handleEdit = () => {
+    setIsInputActive(true);
+  }
+
   return(
     <Draggable key={data.id} draggableId={data.id} index={index}>
       {(provided, snapshot) => 
-          <div
-            className={"todo" + ((snapshot.isDragging) ? " dragging" : "")}
-            ref={provided.innerRef}
-            {...provided.draggableProps}
-            {...provided.dragHandleProps}
-          >
-            <Text
-            text={data.text}
-            onSetText={text => change(areaKey, index, text)}
-           />
-           <button className="action-button remove-button" onClick={() => remove(areaKey, index)}><XSquare /></button>
-            
+        <div
+          className={"todo" + ((snapshot.isDragging) ? " dragging" : "")}
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+        >
+          <div>
+            <span className="inline-text">
+            { isInputActive ? 
+              <input
+                className="text-input"
+                ref={inputRef}
+                value={inputValue}
+                onChange={handleInputChange}
+              />
+              :
+              <span className="text-input" ref={textRef}>
+                {data.text}
+              </span>
+            }  
+            </span>
           </div>
+          <div>
+            { isInputActive ?
+              <button className="action-button save-button" onClick={handleSave}><CheckSquare/></button>
+            :
+              <button className="action-button edit-button" onClick={handleEdit}><Edit/></button>
+            }
+            <button className="action-button remove-button" onClick={() => remove(areaKey, index)}><XSquare /></button>
+          </div>
+        </div>
       }
     </Draggable>
   );
